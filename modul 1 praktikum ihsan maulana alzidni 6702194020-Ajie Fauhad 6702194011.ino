7093f4d307450df78a0e6f9202b85b40d23ebd00
ajie-fauhad <79488337+ajie-fauhad@users.noreply.github.com>
2021-03-09 20:41:57 +0700

    Update modul 1 praktikum ihsan maulana alzidni 6702194020-Ajie Fauhad 6702194011.ino

--- modul 1 praktikum ihsan maulana alzidni 6702194020-Ajie Fauhad 6702194011.ino
+++ modul 1 praktikum ihsan maulana alzidni 6702194020-Ajie Fauhad 6702194011.ino
@@ -1,10 +1,9 @@
-//Modul Praktikum 1-Sistem kendali On/Of
-//Nama Team : DuetMaut
-//Nama Anggota 1 : Ihsan Maulana Alzidni 
-//Nama Anggota 2 : Ajie Fauhad
-//Versi Program : 1.0
+// PRAKTIKUM 1 SISTEM KENDALI ON/OFF
+// KELOMPOK 17
+// ANGGOTA : AJIE FAUHAD FADHLULLAH (6702194011)
+// ANGGOTA : IHSAN MAULANA (6702194020)
 
-//pin buat sensor analog
+//Deklarasi Pin yang digunakan untuk sensor
 int sensor1 = A0;
 int sensor2 = A1;
 int sensor3 = A2;
@@ -13,97 +12,102 @@ int sensor5 = A4;
 int sensor6 = A5;
 int baca_sensor[6];
 
-//pin buat enable
-int pinEnable =4;
-int pinEnable2 = 2;
+//pin penggerak Motor
+int pinEnable = 4; //Pin 1&2 harus HIGH
+int pinEnable2 = 2; //Pin 3&4 harus HIGH
 
-//pin buat motor kiri
-int motor_kiri1 = 5;
-int motor_kiri2 = 6;
+//pin Motorkiri 
+int motor_kiri1 = 5; //input motor driver
+int motor_kiri2 = 6; //input motor driver
 
-//pin buat motor kanan
+//pin Motorkanan
 int motor_kanan1 = 3;
 int motor_kanan2 = 11;
 
 void setup(){
-  pinMode(sensor1, INPUT);
-  pinMode(sensor2, INPUT);
-  pinMode(sensor3, INPUT);
-  pinMode(sensor4, INPUT);
-  pinMode(sensor5, INPUT);
-  pinMode(sensor6, INPUT);
-   
-  pinMode(pinEnable, OUTPUT);
-  pinMode(pinEnable2, OUTPUT);
-  pinMode(motor_kiri1, OUTPUT);
-  pinMode(motor_kiri2, OUTPUT);
-  pinMode(motor_kanan1, OUTPUT);
-  pinMode(motor_kanan2, OUTPUT);
-  
-  Serial.begin(9600);
+// Keenam Sensor Photodiode sebagai INPUT yaitu sensor cahaya
+pinMode(sensor1, INPUT);
+pinMode(sensor2, INPUT);
+pinMode(sensor3, INPUT);
+pinMode(sensor4, INPUT);
+pinMode(sensor5, INPUT);
+pinMode(sensor6, INPUT);
+
+  //Motor sebagai OOUTPUT pengerak
+pinMode(pinEnable, OUTPUT);
+pinMode(pinEnable2, OUTPUT);
+pinMode(motor_kiri1, OUTPUT);
+pinMode(motor_kiri2, OUTPUT);
+pinMode(motor_kanan1, OUTPUT);
+pinMode(motor_kanan2, OUTPUT);
+
+Serial.begin(9600);
 }
 
-void readsensor() { 
-  baca_sensor[0] = analogRead(sensor1);
-  baca_sensor[1] = analogRead(sensor2);
-  baca_sensor[2] = analogRead(sensor3);
-  baca_sensor[3] = analogRead(sensor4);
-  baca_sensor[4] = analogRead(sensor5);
-  baca_sensor[5] = analogRead(sensor6);
+//Membaca sinyal analog dari sensor
+void readsensor(){
+ 	baca_sensor[0] = analogRead(sensor1); 
+	baca_sensor[1] = analogRead(sensor2); 
+  	baca_sensor[2] = analogRead(sensor3);
+  	baca_sensor[3] = analogRead(sensor4); 
+  	baca_sensor[4] = analogRead(sensor5);
+  	baca_sensor[5] = analogRead(sensor6); 
   
   Serial.println(baca_sensor[0]);
 }
 
 void loop(){
-    
-    readsensor();
-    if (baca_sensor[0] < 34 && baca_sensor[1] < 34 && 
-        baca_sensor[2] > 34 && baca_sensor[3] > 34 && 
-        baca_sensor[4] > 34 && baca_sensor[5] > 34){
-        //motor kanan aktif
-      	digitalWrite(pinEnable, HIGH);
-        digitalWrite(motor_kiri1, LOW);
-        digitalWrite(motor_kiri2, LOW);
-        
-        digitalWrite(pinEnable2, HIGH);
-        digitalWrite(motor_kanan1, HIGH);
-        digitalWrite(motor_kanan2, LOW);
-    }
-    else if (baca_sensor[0] > 34 && baca_sensor[1] > 34 && 
-             baca_sensor[2] < 34 && baca_sensor[3] < 34 && 
-             baca_sensor[4] > 34 && baca_sensor[5] > 34){
-      	//kedua motor aktif
-        digitalWrite(pinEnable, HIGH);
-        digitalWrite(motor_kiri1, HIGH);
-        digitalWrite(motor_kiri2, LOW);
-        
-        digitalWrite(pinEnable2, HIGH);
-        digitalWrite(motor_kanan1, HIGH);
-        digitalWrite(motor_kanan2, LOW);
-    }
-    else if (baca_sensor[0] > 34 && baca_sensor[1] > 34 && 
-             baca_sensor[2] > 34 && baca_sensor[3] > 34 && 
-             baca_sensor[4] < 34 && baca_sensor[5] < 34){
-      	//motor kiri aktif
-        digitalWrite(pinEnable, HIGH);
-        digitalWrite(motor_kiri1, HIGH);
-        digitalWrite(motor_kiri2, LOW);
-        
-        digitalWrite(pinEnable2, HIGH);
-        digitalWrite(motor_kanan1, LOW);
-        digitalWrite(motor_kanan2, LOW);
-    }
-    else if (baca_sensor[0] > 34 && baca_sensor[1] > 34 && 
-             baca_sensor[2] > 34 && baca_sensor[3] > 34 && 
-             baca_sensor[4] > 34 && baca_sensor[5] > 34){
-      	//STOP CONDITION
-        digitalWrite(pinEnable, HIGH);
-        digitalWrite(motor_kiri1, LOW);
-        digitalWrite(motor_kiri2, LOW);
-        
-        digitalWrite(pinEnable2, HIGH);
-        digitalWrite(motor_kanan1, LOW);
-        digitalWrite(motor_kanan2, LOW);
-    }
-  else{}
+	readsensor();
+  //kondisi 1 jika sensor 1 dan 2 mendeteksi gelap maka motor kanan menyala
+  if(baca_sensor[0] < 34 && baca_sensor[1] < 34  && baca_sensor[2] > 34 && baca_sensor[3] > 34 && baca_sensor[4] > 34 && baca_sensor[5] > 34)
+  {
+ 	digitalWrite(pinEnable, HIGH);
+  	digitalWrite(motor_kiri1,LOW);  
+    digitalWrite(motor_kiri2,LOW);
+    digitalWrite(pinEnable2, HIGH);
+  	digitalWrite(motor_kanan1,HIGH);  
+    digitalWrite(motor_kanan2,LOW);
+  }
+  //kondisi 2 jika sensor 3 dan 4 mendeteksi gelap maka motor kiri dan kanan menyala
+  else if(baca_sensor[0] > 34 && baca_sensor[1] > 34  && baca_sensor[2] < 34 && baca_sensor[3] < 34 && baca_sensor[4] > 34 && baca_sensor[5] > 34)
+  {
+ 	digitalWrite(pinEnable, HIGH);
+  	digitalWrite(motor_kiri1,HIGH);  
+    digitalWrite(motor_kiri2,LOW);
+    digitalWrite(pinEnable2, HIGH);
+  	digitalWrite(motor_kanan1,HIGH);  
+    digitalWrite(motor_kanan2,LOW);
+  }
+  //kondisi 3 jika sensor 5 dan 6 mendeteksi gelap maka motor kiri menyala
+  else if(baca_sensor[0] > 34 && baca_sensor[1] > 34  && baca_sensor[2] > 34 && baca_sensor[3] > 34 && baca_sensor[4] < 34 && baca_sensor[5] < 34)
+  {
+ 	digitalWrite(pinEnable, HIGH);
+  	digitalWrite(motor_kiri1,HIGH);  
+    digitalWrite(motor_kiri2,LOW);
+    digitalWrite(pinEnable2, HIGH);
+  	digitalWrite(motor_kanan1,LOW);  
+    digitalWrite(motor_kanan2,LOW);
+  }
+  //kondisi 4 jika ke-6 sensor diterangkan semua maka motor tidak akan menyala
+  else {
+    digitalWrite(pinEnable, HIGH);
+  	digitalWrite(motor_kiri1,LOW);  
+    digitalWrite(motor_kiri2,LOW);
+    digitalWrite(pinEnable2, HIGH);
+  	digitalWrite(motor_kanan1,LOW);  
+    digitalWrite(motor_kanan2,LOW);
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
